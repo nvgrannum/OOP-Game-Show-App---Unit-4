bc0bac6bb7deba5c9fcd8ff2b174afa602fd223b
nvgrannum <79998374+nvgrannum@users.noreply.github.com>
2021-04-05 17:33:57 -0700

    Update Phrase.js

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -13,24 +13,18 @@ class Phrase {
 
     addPhraseToDisplay() {
         let phraseLength = this.phrase.length;
-        // let phraseLength = [...phrase].map(letter => {
-        //     if(/^[a-z]+$/.test(phraseLength)) {
-        //     list.createElement('li.letter')
-        //     } else if () {}
-        // });
 
         for (let i=0; i < phraseLength; i++) {
            if (/[a-z]/i.test(this.phrase.charAt(i))) {
             list.appendChild(createLi).classList.add(`hide_letter_${this.phrase.charAt(i)}`);
             
-        }  else if (/\s/.test(this.phrase.charAt(i))) {
+            
+        } else if (/\s/.test(this.phrase.charAt(i))) {
             list.appendChild(createLi).classList.add('space');
         } else {
             alert('you have no idea what is going on do you');
         }
-        
-    
-         };
+        };
          console.log(phraseLength);
     }
 
